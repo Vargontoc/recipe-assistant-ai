326884d0af80b3eadd9b45607fd2bef3b9a54795
Vargontoc <atperamato@gmail.com>
2025-08-21 21:03:16 +0200

    chore: Initial proyect with backend, frontend and basic configuration

---
+++ backend/recipe-assistant-api/src/main/java/es/agonzalez/assistant/recipe/es/agonzalez/assistant/recipe/api/AssistantRecipeAiApplication.java
@@ -0,0 +1,13 @@
+package es.agonzalez.assistant.recipe.es.agonzalez.assistant.recipe.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantRecipeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantRecipeAiApplication.class, args);
+	}
+
+}
---
+++ backend/recipe-assistant-api/src/test/java/es/agonzalez/assistant/recipe/es/agonzalez/assistant/recipe/api/AssistantRecipeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.agonzalez.assistant.recipe.es.agonzalez.assistant.recipe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantRecipeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
